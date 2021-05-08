412dc1ad26c9c10d4988ba1c2458e98742eb8bdd
ftees <tiennp8@gmail.com>
2021-05-08 20:11:05 +0700

    updated avt

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -6,7 +6,7 @@ const about = () => {
             <h2>About me</h2>
             <div className="content">
             <div className="content-img">
-                <img src="avt.jpg"></img>
+                <img src="av.jpg"></img>
             </div>
             <div className="content-intro">
                 <h1>Nguyễn Phạm Tiến</h1>
--- src/components/skills/skill.js
+++ src/components/skills/skill.js
@@ -2,30 +2,43 @@ import React from 'react'
 import './skill.css'
 import ProgressBar from "@ramonak/react-progress-bar";
 const skill = () => {
+    let skills = [
+      {
+        name: "HTML",
+        percent: 80,
+
+      },
+      {
+        name: "CSS",
+        percent: 70,
+
+      },
+      {
+        name: "JavaScript",
+        percent: 55,
+
+      },
+      {
+        name: "Soft Skill",
+        percent: 75,
+
+      },
+      {
+        name: "Office Skill",
+        percent: 80,
+
+      },
+  ]
     return (
         <div className="skill-contain">
             <h2>Skills</h2>
             <div className="list-Skills">
-            <div className="skill">
-                <h3>HTML</h3>
-              <div className="skill-bar"><ProgressBar completed={80} height={22} isLabelVisible={false}>HTML</ProgressBar></div>
-             </div>
-             <div className="skill">
-                <h3>CSS</h3>
-              <div className="skill-bar"><ProgressBar completed={70} height={22} isLabelVisible={false}>HTML</ProgressBar></div>
-             </div>
-             <div className="skill">
-                <h3>JavaScript</h3>
-              <div className="skill-bar"><ProgressBar completed={55} height={22} isLabelVisible={false}>HTML</ProgressBar></div>
-             </div>
-             <div className="skill">
-                <h3>Soft Skill</h3>
-              <div className="skill-bar"><ProgressBar completed={75} height={22} isLabelVisible={false}>HTML</ProgressBar></div>
-             </div>
-             <div className="skill">
-                <h3>Office Skill</h3>
-              <div className="skill-bar"><ProgressBar completed={80} height={22} isLabelVisible={false}>HTML</ProgressBar></div>
-             </div>
+              {skills.map(item =>
+                  <div className="skill">
+                  <h3>{item.name}</h3>
+                <div className="skill-bar"><ProgressBar completed={item.percent} height={22} isLabelVisible={false}>HTML</ProgressBar></div>
+                </div>
+              )}
              </div>
         </div>
     )
