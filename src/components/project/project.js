e2d9d51ef81f0366df6f14b43fa7a2cdc55f3649
ftees <tiennp8@gmail.com>
2021-06-27 20:55:54 +0700

    update project

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -1,40 +1,42 @@
-import React from 'react'
-import './project.css'
+import React from "react";
+import "./project.css";
 const project = () => {
-    let projects = [
+  let projects = [
     {
-        title: "Portfolio Website with React",
-        img: "cv1.jpg",
-        link:"https://ftees.github.io/Portfolio-By-React/"
-        },
+      title: "Portfolio Website by React",
+      img: "cv1.jpg",
+      link: "https://ftees.github.io/Portfolio-By-React/",
+    },
     {
-        title: "Weather App with React",
-        img: "weather.jpg",
-        link:"https://ftees.github.io/Weather-App-by-React/"
-        },
-        {
-            title: "E-Commerce Website with Angular",
-            img: "ecm.png",
-            link:"https://www.w3schools.com/howto/howto_css_animate_buttons.asp"
-            }
+      title: "Weather App by React",
+      img: "weather.jpg",
+      link: "https://ftees.github.io/Weather-App-by-React/",
+    },
+    {
+      title: "Movie App by React",
+      img: "movie.jpg",
+      link: "https://ftees.github.io/Movie-app/",
+    },
+  ];
+  return (
+    <div className="project-contain">
+      <h2>Projects</h2>
+      <div className="list-projects container">
+        {projects.map((item) => (
+          <div className="project">
+            <img className="project-background" src={item.img}></img>
 
-    ]
-    return (
-        <div className="project-contain">
-            <h2>Projects</h2>
-            <div className="list-projects container">
-                {projects.map(item => (
-                    <div className="project">
-                    <img className="project-background" src={item.img}></img>
-                    
-                    <h5>{item.title}</h5>
-                    <button className="btn-view"><a href={item.link} target="_blank">View</a></button>
-                    
-                </div>
-                ))} 
-            </div>
-        </div>
-    )
-}
+            <h5>{item.title}</h5>
+            <button className="btn-view">
+              <a href={item.link} target="_blank">
+                View
+              </a>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default project
+export default project;
